b1008f1264211620c47b27c57d48b49f599bc79f
Suresh Parimi <42301301+sureshparimi@users.noreply.github.com>
2023-07-18 16:30:53 +0530

    Add files via upload

--- scratchpad.py
+++ scratchpad.py
@@ -1,41 +1,80 @@
+import shutil
 import json
 import time
-from datetime import datetime
+from datetime import date
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 from locators import Locators
 
-# Set up Chrome options
-options = Options()
-options.add_argument("start-maximized")
-options.add_argument("disable-infobars")
-options.add_argument("--disable-extensions")
-options.add_argument("--disable-dev-shm-usage")
-options.add_argument("--no-sandbox")
-options.add_argument('--headless')
-options.binary_location = "/usr/bin/chromium-browser"
+def copy_data(source_file, destination_file):
+    with open(source_file, 'r') as source:
+        source_data = json.load(source)
+    
+    with open(destination_file, 'r') as destination:
+        destination_data = json.load(destination)
+    
+    destination_data.extend(source_data)
+    
+    with open(destination_file, 'w') as destination:
+        json.dump(destination_data, destination, indent=4)
+
+source_file = 'ottacomdata.json'
+destination_file = 'masterdatabase.json'
+
+# Append preexisting content from ottacomdata.json to masterdatabase.json
+copy_data(source_file, destination_file)
+
+def remove_duplicates(input_file, output_file):
+    with open(input_file, 'r') as file:
+        data = json.load(file)
+
+    unique_joblinks = set()
+    unique_records = []
+    removed_records = []
+
+    for job in data:
+        joblink = job['JobLink']
+        if joblink not in unique_joblinks:
+            unique_joblinks.add(joblink)
+            unique_records.append(job)
+        else:
+            removed_records.append(job)
+
+    with open(output_file, 'w') as file:
+        json.dump(unique_records, file, indent=4)
+
+    with open("removed_duplicate_records.json", 'w') as removed_file:
+        json.dump(removed_records, removed_file, indent=4)
+
+    print("Removed Duplicate Records:")
+    for removed_record in removed_records:
+        print(removed_record)
+
+def click_element(driver, locator):
+    element = driver.find_element(By.XPATH, locator)
+    if element.is_displayed():
+        actions = ActionChains(driver)
+        actions.move_to_element(element).click().perform()
+    else:
+        print("Element is not displayed.")
 
 # Set up Chrome options
-options = Options()
-options.add_argument("start-maximized")
-options.add_argument("disable-infobars")
-options.add_argument("--disable-extensions")
-options.add_argument("--disable-dev-shm-usage")
-options.add_argument("--no-sandbox")
-options.add_argument('--headless')
-options.binary_location = "/usr/bin/chromium-browser"
+chrome_options = Options()
+# Uncomment the line below if you want to run Chrome in headless mode
+# chrome_options.add_argument("--headless")
 
-# # Set up Chrome driver service
-# service = Service(ChromeDriverManager().install())
+# Set up Chrome driver service
+service = Service(ChromeDriverManager().install())
 
 # Set up Chrome driver
-driver = webdriver.Chrome(options=options)
+driver = webdriver.Chrome(service=service, options=chrome_options)
 
 # Maximize the browser window
 driver.maximize_window()
@@ -44,8 +83,7 @@ driver.maximize_window()
 driver.get('https://www.otta.com')
 
 # Click on the login button
-element = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, Locators.LOGIN_BUTTON)))
-element.click()
+click_element(driver, Locators.LOGIN_BUTTON)
 
 # Load test data from JSON file
 with open('test_data.json') as json_file:
@@ -60,21 +98,27 @@ password_field.send_keys(test_data['password'])
 
 # Submit the login form
 password_field.submit()
+WebDriverWait(driver, 5)
+
+# Wait for the page to load completely
 WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, Locators.HOME_BUTTON)))
 
 # Click on the "Home" button
-element = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, Locators.HOME_BUTTON)))
-element.click()
+click_element(driver, Locators.HOME_BUTTON)
 
 time.sleep(5)
 
 # Check if "See More" button exists
-see_more_button = driver.find_element(By.XPATH, Locators.SEE_MORE_BUTTON) if driver.find_elements(By.XPATH, Locators.SEE_MORE_BUTTON) else None
+see_more_button = None
+try:
+    see_more_button = driver.find_element(By.XPATH, Locators.SEE_MORE_BUTTON)
+except:
+    pass
 
 # Perform actions based on the availability of the "See More" button
 if see_more_button:
     see_more_button.click()
-    time.sleep(2)
+    time.sleep(2)  # Add a small delay after clicking "See More" button
 
 # Define a list of job data cards
 job_data_cards = [
@@ -87,24 +131,20 @@ job_data_cards = [
     Locators.ALL_MATCHES,
 ]
 
-# Create an empty list to store job data
-job_data_list = []
-
 # Iterate through the list of job data cards and click on each element
 for job_data_card in job_data_cards:
     print("############ Scraping jobs for job card:", job_data_card, "###############################")
     try:
         job_data_card_link = driver.find_element(By.XPATH, job_data_card)
         job_data_card_link.click()
-        time.sleep(5)
-
-        # Check if modal window is present and close it
+        time.sleep(5) # Add a small delay between each action
+        # Add a small delay before closing the modal window
         try:
-            modal_remove_button = driver.find_element(By.XPATH, Locators.MODAL_REMOVE_BUTTON)
-            modal_remove_button.click()
+            MODAL_REMOVE_BUTTON = driver.find_element(By.XPATH,Locators.MODAL_REMOVE_BUTTON)
+            if MODAL_REMOVE_BUTTON:
+                MODAL_REMOVE_BUTTON.click()
         except:
             pass
-
         # Get all the elements matching the locator data-testid="page-progress-dot"
         page_progress_dots = driver.find_elements(By.XPATH, Locators.progress_dot_count)
 
@@ -116,20 +156,22 @@ for job_data_card in job_data_cards:
                 job_title_text = job_title_element.text
                 job_location_element = driver.find_element(By.XPATH, Locators.job_location)
                 job_location_text = job_location_element.text
-
-                # Click on the "Apply" button
+                job_posted = datetime.now().strftime("%d %B %Y %I:%M %p %Z")
+                # driver.refresh()
+                time.sleep(1)
+                # Locate the Apply button
                 element = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//button[@data-testid="apply-button"]/descendant::p[text()="Apply"]')))
                 element.click()
+                print("this is after the click of Apply button")
                 time.sleep(3)
 
-                # Check if "Apply" content is present
                 try:
-                    apply_content_job_link_element = driver.find_element(By.XPATH, Locators.apply_content_job_link)
-                    JobLink = apply_content_job_link_element.get_attribute('href')
+                    Apply_content_job_link_element = driver.find_element(By.XPATH, Locators.apply_content_job_link)
+                    JobLink = Apply_content_job_link_element.get_attribute('href')
                 except:
                     try:
-                        apply_via_otta_link_element = driver.find_element(By.XPATH, '//*[@data-testid="apply-content"]//a')
-                        JobLink = apply_via_otta_link_element.get_attribute('href')
+                        Apply_via_otta_link_element = driver.find_element(By.XPATH, '//*[@data-testid="apply-content"]//a')
+                        JobLink = Apply_via_otta_link_element.get_attribute('href')
                     except:
                         try:
                             dialog_close_button_element = driver.find_element(By.XPATH, Locators.dialog_close_button)
@@ -138,39 +180,49 @@ for job_data_card in job_data_cards:
                             pass
 
                         # Get the job title and link from the <h2> element
+                        job_title_element = driver.find_element(By.XPATH, Locators.job_title)
                         job_link_element = job_title_element.find_element(By.TAG_NAME, 'a')
                         JobLink = job_link_element.get_attribute('href')
 
                         # Set the focus on the main page
                         driver.switch_to.default_content()
 
-                # Append job data to the list
-                job_data = {
-                    "job_title": job_title_text,
-                    "job_location": job_location_text,
-                    "JobLink": JobLink,
-                    "job_posted": datetime.now().strftime("%d %B %Y %I:%M %p %Z")
-                }
-                job_data_list.append(job_data)
+                    # Append job_title, job_location_text, and JobLink to ottacomdata.json file
+                    job_data_list = []
+                    with open('ottacomdata.json', 'a+') as json_file:
+                        json_file.seek(0)
+                        file_content = json_file.read()
+                        if file_content:
+                            job_data_list = json.loads(file_content)
 
-                # Close the dialog if present
+                        job_data = {
+                            "job_title": job_title_text,
+                            "job_location": job_location_text,
+                            "Job_link": JobLink,
+                            "Job_Posted":job_posted
+                        }
+                        job_data_list.append(job_data)
+
+                        json_file.seek(0)
+                        json_file.truncate(0)
+                        json.dump(job_data_list, json_file, indent=4)
+
                 try:
                     dialog_close_button_element = driver.find_element(By.XPATH, Locators.dialog_close_button)
                     dialog_close_button_element.click()
                 except:
                     pass
 
-                # Go to the next page
+                WebDriverWait(driver,3)
                 next_button = driver.find_element(By.XPATH, Locators.next_button_xpath)
                 next_button.click()
-                time.sleep(3)
-
-                # Check if "Great progress!" message is displayed
+                time.sleep(5)
                 try:
                     element = driver.find_element(By.XPATH, '//h1[contains(text(), "Great progress!")]')
                     if element.is_displayed():
-                        element = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, Locators.HOME_BUTTON)))
-                        element.click()
+                        click_element(driver, Locators.HOME_BUTTON)
+                    else:
+                        pass
                 except:
                     pass
             except Exception as e:
@@ -183,37 +235,45 @@ for job_data_card in job_data_cards:
         print(e)
         continue
 
-    # Click on the "Home" button
-    element = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, Locators.HOME_BUTTON)))
-    element.click()
-
-    # Check if "See Less" button exists
-    see_less_button = driver.find_element(By.XPATH, Locators.SEE_LESS_BUTTON) if driver.find_elements(By.XPATH, Locators.SEE_LESS_BUTTON) else None
+    click_element(driver, Locators.HOME_BUTTON)
+     # Check if "See Less" button exists
+    see_less_button = None
+    try:
+        see_less_button = driver.find_element(By.XPATH, Locators.SEE_LESS_BUTTON)
+    except:
+        pass
 
     # Perform actions based on the availability of the "See Less" button
     if see_less_button:
         see_less_button.click()
-        time.sleep(5)
+        time.sleep(5)  # Add a small delay after clicking "See Less" button
 
     # Check if "See More" button exists
-    see_more_button = driver.find_element(By.XPATH, Locators.SEE_MORE_BUTTON) if driver.find_elements(By.XPATH, Locators.SEE_MORE_BUTTON) else None
+    see_more_button = None
+    try:
+        see_more_button = driver.find_element(By.XPATH, Locators.SEE_MORE_BUTTON)
+    except:
+        pass
 
     # Perform actions based on the availability of the "See More" button
     if see_more_button:
         see_more_button.click()
-        time.sleep(5)
+        time.sleep(5)  # Add a small delay after clicking "See More" button
+
+# Wait for user input before closing the browser
+# input("Press any key to close the browser...")
 
 # Close the browser
 driver.quit()
 
-# Write job data to ottacomdata.json file
-with open('ottacomdata.json', 'a') as json_file:
-    json.dump(job_data_list, json_file, indent=4)
-
-# Read job data from ottacomdata.json
+# Append preexisting content from ottacomdata.json to masterdatabase.json
 with open('ottacomdata.json') as ottacom_file:
     ottacom_data = json.load(ottacom_file)
 
-# Append job data to masterdatabase.json
 with open('masterdatabase.json', 'a') as master_file:
     json.dump(ottacom_data, master_file, indent=4)
+
+# Call remove_duplicates() function
+input_file = 'masterdatabase.json'
+output_file = 'UniqueJobsdataMasterFile.json'
+remove_duplicates(input_file, output_file)
